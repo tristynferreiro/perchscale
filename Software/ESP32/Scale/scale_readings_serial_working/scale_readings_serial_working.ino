069615ddd9743cda4bdd5e7d1545e4116d57cf6f
tristynferreiro <tferreiro1@gmail.com>
2024-03-29 11:48:47 +0200

    ESP32: working scale script with calibration, and printing to serial

--- Software/ESP32/Scale/scale_readings_serial_working/scale_readings_serial_working.ino
+++ Software/ESP32/Scale/scale_readings_serial_working/scale_readings_serial_working.ino
@@ -1,53 +1,40 @@
 /*
-   -------------------------------------------------------------------------------------
-   HX711_ADC
-   Arduino library for HX711 24-Bit Analog-to-Digital Converter for Weight Scales
-   Olav Kallhovd sept2017
-   -------------------------------------------------------------------------------------
+* This the the Scale UART sender project. It reads the scale data and transmits it over UART
+* First perch-scale submodule (scale and data/microcontroller) integration test 
 */
 
-/*
-   This example file shows how to calibrate the load cell and optionally store the calibration
-   value in EEPROM, and also how to change the value manually.
-   The result value can then later be included in your project sketch or fetched from EEPROM.
-
-   To implement calibration in your project sketch the simplified procedure is as follow:
-       LoadCell.tare();
-       //place known mass
-       LoadCell.refreshDataSet();
-       float newCalibrationValue = LoadCell.getNewCalibration(known_mass);
-*/
-
+//------------------INCLUDES------------------------
 #include <HX711_ADC.h>
 #if defined(ESP8266)|| defined(ESP32) || defined(AVR)
 #include <EEPROM.h>
 #endif
 
-/****************************************/
-/********** GLOBAL PARAMETERS ***********/
-// Setup I2C -- HX711 pins:
+//--------------------DEFINES-------------------------
+#define eigthseconds (millis()/125) // Taring delay
+
+//--------------------GLOBAL VARIABLES-----------------
+// pins:
 const int HX711_dout = 32; // D32 mcu > HX711 dout pin 
 const int HX711_sck = 33; // D33 mcu > HX711 sck pin
 
-// Create HX711 object (to use the library)
-HX711_ADC LoadCell(HX711_dout, HX711_sck);
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck); // make an HX711 object
 
+// EEPROM address
 const int calVal_eepromAdress = 0;
-unsigned long t = 0;
-/****************************************/
-/****************************************/
+
+// scale readings counter
+int num_readings = 0;
+//------------------------------------------------------
+
 
 void setup() {
-  /* SETUP ROUTINE */
-  Serial.begin(9600); delay(10); // Setup UART connection
+  // SERIAL SETUP
+  Serial.begin(9600); // Initialise baud rate with PC
   
-  
-  Serial.println();
-  Serial.println("Starting...");
-
+  // LOADCELL SETUP
   LoadCell.begin();
   // LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
-
   unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
   boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
   LoadCell.start(stabilizingtime, _tare);
@@ -56,14 +43,63 @@ void setup() {
     Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
     while (1);
   }
-  else {
-    LoadCell.setCalFactor(1.0); // user set calibration value (float), initial value 1.0 may be used for this sketch
-    Serial.println("Startup is complete");
-  }
+  // else {
+  //   LoadCell.setCalFactor(1.0); // user set calibration value (float), initial value 1.0 may be used for this sketch
+  //   Serial.println("Startup is complete");
+  // }
+  Serial.println("Startup is complete");
   while (!LoadCell.update());
   calibrate(); //start calibration procedure
 }
 
+void loop() {
+
+  static boolean newDataReady = 0;
+
+  // check for new data/start next conversion:
+  if (LoadCell.update()) newDataReady = true;
+
+  if (newDataReady) {
+      float reading = LoadCell.getData();
+      if(reading<1 & eigthseconds%80 == 0){ //Condition to set new tare every 1000 milliseconds
+        boolean _resume = false;
+        boolean _tarewait = true;
+        while (_resume == false) {
+          LoadCell.update();
+          if(_tarewait){              
+            LoadCell.tareNoDelay();
+            _tarewait = false;
+          }
+          if (LoadCell.getTareStatus() == true) {
+            Serial.println("Next Tare Complete");
+            _resume = true;
+          }
+        }
+      }
+      else if (reading>1){ //If a bird or heavy object is detected on the scale
+         Serial.println(String(num_readings) + "," + String(reading));
+          newDataReady = 0;
+          num_readings++;
+      }
+    
+  }
+
+  // receive command from serial terminal
+  if (Serial.available() > 0) {
+    char inByte = Serial.read();
+    if (inByte == 't') LoadCell.tareNoDelay(); //tare
+    else if (inByte == 'r') calibrate(); //calibrate
+    else if (inByte == 'c') changeSavedCalFactor(); //edit calibration value manually
+  }
+
+  // check if last tare operation is complete
+  if (LoadCell.getTareStatus() == true) {
+    Serial.println("Tare complete");
+  }
+
+}
+
+//------------------HELPER FUNCTIONS------------------------
 void calibrate() {
   Serial.println("***");
   Serial.println("Start calibration:");
@@ -196,72 +232,4 @@ void changeSavedCalFactor() {
   }
   Serial.println("End change calibration value");
   Serial.println("***");
-}
-
-/****************************************/
-/************* Main *********************/
-
-int count = 0;
-double scaleReadings[100];
-void loop() {
-  static boolean newDataReady = 0;
-  const int serialPrintInterval = 0; //increase value to slow down serial print activity
-
-  // check for new data/start next conversion:
-  if (LoadCell.update()) newDataReady = true;
-
-
-  // get smoothed value from the dataset:
-  if (newDataReady) {
-    if (millis() > t + serialPrintInterval) {
-      float i = LoadCell.getData();
-      if(i<100 & millis()%100 == 0){ //Condition to set new tare every 100 milliseconds
-        boolean _resume = false;
-        boolean _tarewait = true;
-        while (_resume == false) {
-          LoadCell.update();
-          if(_tarewait){              
-            LoadCell.tareNoDelay();
-            _tarewait = false;
-          }
-          //Serial.println("In the tare");
-          if (LoadCell.getTareStatus() == true) {
-            Serial.println("Next Tare Complete");
-            _resume = true;
-          }
-        }
-        memset(scaleReadings, 0, sizeof(scaleReadings));
-        count = 0;
-      }
-      else if (i>10){ //If a bird or heavy object is detected on the scale
-        Serial.print("Load_cell output val: ");
-        Serial.println(i);
-        Serial.println(", counter = ");
-        Serial.println(count);
-        newDataReady = 0;
-        t = millis();
-        scaleReadings[count] = i;
-        count++;
-      }
-      else{ 
-        newDataReady = 0;
-        memset(scaleReadings, 0, sizeof(scaleReadings));
-        count = 0;
-      }
-    }
-  }
-    
-  // receive command from serial terminal
-  if (Serial.available() > 0) {
-    char inByte = Serial.read();
-    if (inByte == 't') LoadCell.tareNoDelay(); //tare
-    else if (inByte == 'r') calibrate(); //calibrate
-    else if (inByte == 'c') changeSavedCalFactor(); //edit calibration value manually
-  }
-
-  // check if last tare operation is complete
-  if (LoadCell.getTareStatus() == true) {
-    Serial.println("Tare complete");
-  }
-
 }
