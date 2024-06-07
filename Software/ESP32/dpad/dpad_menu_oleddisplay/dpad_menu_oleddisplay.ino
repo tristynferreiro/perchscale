9ea4838f051442741f02cdb6e4c355bf28918a12
JNSRYA006 <JNSRYA006@myuct.ac.za>
2024-06-07 15:39:38 +0200

    DPAD: Working function to detect state of all 4 butotns and display on OLED display

---
+++ Software/ESP32/dpad/dpad_menu_oleddisplay/dpad_menu_oleddisplay.ino
@@ -0,0 +1,39 @@
+/*
+* WRITE DESCRIPTIONS
+*
+* Working: DATE of last working
+*
+* Pin connections:
+*   None
+*/
+
+//------------------INCLUDES------------------------
+// Add your includes here
+//--------------------------------------------------
+
+
+//--------------------DEFINES-------------------------
+// Add your global defines here
+//----------------------------------------------------
+
+//--------------------GLOBAL VARIABLES-----------------
+// Add your global variables here
+//-----------------------------------------------------
+
+//--------------------HELPER FUNCTIONS-----------------
+// Add your helper functions here
+//-----------------------------------------------------
+
+void setup() {
+  //--------------------X SETUP-----------------------
+
+  //--------------------Y SETUP-----------------------
+
+  //--------------------Z SETUP-----------------------
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
