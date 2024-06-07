6e4a4cfadd424e0ecc4420a7234a2ad258b90a24
JNSRYA006 <JNSRYA006@myuct.ac.za>
2024-06-07 12:04:01 +0200

    Display: Hello World scripts for OLED and LCD WORKING

---
+++ Software/ESP32/display/lcd/lcd_write_to_display_working/lcd_write_to_display_working.ino
@@ -0,0 +1,49 @@
+#include <LiquidCrystal_I2C.h>
+
+// set the LCD number of columns and rows
+int lcdColumns = 16;
+int lcdRows = 2;
+
+// set LCD address
+// if you don't know your display address, run an I2C scanner sketch
+int lcdAddr = 0x27;
+
+// set LCD address, number of columns and rows
+LiquidCrystal_I2C lcd(lcdAddr, lcdColumns, lcdRows);  
+
+String messageStatic = "Static message";
+String messageToScroll = "This is a scrolling message with more than 16 characters";
+
+// Function to scroll text
+// The function acepts the following arguments:
+// row: row number where the text will be displayed
+// message: message to scroll
+// delayTime: delay between each character shifting
+// lcdColumns: number of columns of your LCD
+void scrollText(int row, String message, int delayTime, int lcdColumns) {
+  for (int i=0; i < lcdColumns; i++) {
+    message = " " + message;  
+  } 
+  message = message + " "; 
+  for (int pos = 0; pos < message.length(); pos++) {
+    lcd.setCursor(0, row);
+    lcd.print(message.substring(pos, pos + lcdColumns));
+    delay(delayTime);
+  }
+}
+
+void setup(){
+  // initialize LCD
+  lcd.init();
+  // turn on LCD backlight                      
+  lcd.backlight();
+}
+
+void loop(){
+  // set cursor to first column, first row
+  lcd.setCursor(0, 0);
+  // print static message
+  lcd.print(messageStatic);
+  // print scrolling message
+  scrollText(1, messageToScroll, 250, lcdColumns);
+}
