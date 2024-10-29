957c3eec6fcceedee8d3faefc3f942e409647a30
Tristyn Ferreiro <tferreiro1@gmail.com>
2024-10-29 14:02:20 +0100

    Cleaning for v1 deployment

--- EEE4113F/Software/EEE4113F_Code/Web_File_Download_v2/Network.h
+++
@@ -1,5 +0,0 @@
-// Adjust the following values to match your needs
-// -----------------------------------------------
-#define   servername "featherweb"     // Set your server's logical name here e.g. if myserver then address is http://myserver.local/
-
-
--- EEE4113F/Software/EEE4113F_Code/Web_File_Download_v2/Sys_Variables.h
+++
@@ -1,7 +0,0 @@
-String webpage = "";
-bool   SD_present = false;
-
-#define SD_CS_pin           5        // Use pin 5 on MH-T Live ESP32 version of Wemos D1 Mini for SD Card Chip-Select
-
-
-
